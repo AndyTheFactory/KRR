e740df34630e8de422430ba570bb0e9b9ae722d1
Andrei <andrei@thephpfactory.com>
2018-12-28 00:53:32 +0200

    S

--- Lab10/src/lab10/Lab10.java
+++ Lab10/src/lab10/Lab10.java
@@ -5,6 +5,11 @@
  */
 package lab10;
 
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
 /**
  *
  * @author Dell
@@ -14,7 +19,7 @@ public class Lab10 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
         Grid g1,g2,g3;
         g1=new Grid(new int[]{1, 2, 3, 5,2, 2, 1, 2,3, 2, 1, 1,0, 0, 0, 0}, 
@@ -27,7 +32,17 @@ public class Lab10 {
         int[] testi=new int[]{0, 1, 2, 5, 10, 13, 15};
         int[] testj=new int[]{1, 0, 3, 4, 9, 2, 14};
         
-        System.out.println(g1.getProbabilityMatrix()); 
+        double[][] prob=g1.getProbabilityMatrix();
+
+        PrintWriter printWriter = new PrintWriter(new FileWriter("probability.txt"));
+        for(int i=0;i<prob.length;i++){
+            for(int j=0;j<prob[i].length;j++){
+                printWriter.print(prob[i][j]);
+                printWriter.print("\t");
+            }
+            printWriter.print("\r\n");
+        }
+        printWriter.close();
         
     }
     
